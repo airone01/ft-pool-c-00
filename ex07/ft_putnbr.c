7eb6d759e315da47298d8184b7c0f0cb7cf4efa9
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-11 19:37:22 +0200

    feat(ex07): implement

---
+++ ex07/ft_putnbr.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <erwann.lagouche@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 18:01:45 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/11 19:37:16 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+const int int_min = -2147483648;
+const int int_min_edited = 147483648;
+const int int_max = 2147483647;
+
+void	ft_putchar(char c)
+{
+	write (1, &c, 1);
+}
+
+void	ft_putnbr(int nbr)
+{
+	if (nbr == int_min)
+	{
+		ft_putchar ('-');
+		ft_putchar ('2');
+		ft_putnbr (int_min_edited);
+		return ;
+	}
+	if (nbr < 0)
+	{
+		ft_putchar ('-');
+		ft_putnbr (-(nbr));
+		return ;
+	}
+	if (nbr >= 10)
+	{
+		ft_putnbr (nbr / 10);
+		ft_putchar ((nbr % 10) + '0');
+		return ;
+	}
+	ft_putchar (nbr + '0');
+}
+
+// int	main(void)
+// {
+// 	// ft_putnbr (-420);
+// 	ft_putnbr (int_min);
+// 	// ft_putnbr (int_max);
+// }

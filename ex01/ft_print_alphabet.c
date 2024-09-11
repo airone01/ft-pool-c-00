e3dc373c5934a4e663db8a47ca7d22dc5de0eded
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-11 13:40:15 +0200

    feat(ex01): implement

---
+++ ex01/ft_print_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <erwann.lagouche@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 13:22:44 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/11 13:39:12 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	ch;
+
+	ch = 'a';
+	while (ch <= 'z')
+	{
+		write (1, &ch, 1);
+		ch++;
+	}
+}
+
+int	main (void)
+{
+	ft_print_alphabet ();
+}

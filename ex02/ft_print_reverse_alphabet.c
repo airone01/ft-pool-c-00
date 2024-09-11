338913e86d2cb74979e15fd184162d546b821018
Erwann Lagouche <erwann.lagouche@gmail.com>
2024-09-11 13:50:37 +0200

    feat(ex02): implement

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elagouch <erwann.lagouche@gmail.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/11 13:42:54 by elagouch          #+#    #+#             */
+/*   Updated: 2024/09/11 13:50:11 by elagouch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	ch;
+
+	ch = 'z';
+	while (ch >= 'a')
+	{
+		write (1, &ch, 1);
+		ch--;
+	}
+}
+
+int	main(void)
+{
+	ft_print_reverse_alphabet ();
+}
